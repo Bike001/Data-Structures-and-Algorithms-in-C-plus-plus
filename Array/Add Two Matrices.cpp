7494e88452fc49cc2f0a68705edbb3fbc1f50d67
YuSoo <yusoo@YuSoodeMBP.lan>
2023-09-13 00:01:23 -0700

    code revised in matrix array

--- Array/Add Two Matrices.cpp
+++ Array/Add Two Matrices.cpp
@@ -1,69 +1,33 @@
-//This is a C++ Program to Find the Largest and Smallest Elements in an Array.
-//
-//Problem Description
-//The program takes an array and prints the largest and smallest element in the array.
-//
-//Problem Solution
-//1. The program takes an array of elements.
-//2. Using a for loop, the largest and smallest element is found.
-//3. The result is printed.
-//4. Exit.
-
-
-
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-    
-    
-    
-    int arr[10], n, i, max, min;
-    
-    cout<< "Enter the size of array less than 10" << endl;
-    cin >> n;
-    while  (n > 10){
-        
-        cout << "Enter the size of array less than 10 " << endl;
+int main ()
+{
+    int m, n, p, q, i, j, A[5][5], B[5][5], C[5][5];
+    cout << "Enter rows and column of matrix A : ";
+    cin >> m >> n;
+    cout << "Enter rows and column of matrix B : ";
+    cin >> p >> q;
+    if ((m != p) && (n != q))
+    {
+        cout << "Matrices cannot be added!";
+        exit(0);
     }
-    
-    cout << "Enter the elements of array " << endl;
-    
-    for(i=0; i< n; i++){
-        
-        cin >> arr[i];
-        
+    cout << "Enter elements of matrix A : ";
+    for (i = 0; i < m; i++)
+        for (j = 0; j < n; j++)
+            cin >> A[i][j];
+    cout << "Enter elements of matrix B : ";
+    for (i = 0; i < p; i++)
+        for (j = 0; j < q; j++)
+            cin >> B[i][j];
+    for (i = 0; i < m; i++)
+        for (j = 0; j < n; j++)
+            C[i][j] = A[i][j] + B[i][j];
+    cout << "Sum of matrices\n";
+    for (i = 0; i < m; i++)
+    {    for (j = 0; j < n; j++)
+            cout << C[i][j] << "  ";
+        cout << "\n";
     }
-    
-    cout << "print the array" << endl;
-    for (i=0; i< n; i++){
-        
-        cout << arr[i] <<" " << endl;
-        
-    }
-    
-    max = arr[0];
-    for (i=0; i< n; i++){
-        
-        if (max < arr[i])
-            max = arr[i];
-        
-    }
-    
-    cout << " the max number is " << max << endl;
-    
-    min = arr[0];
-    for (i=0; i< n; i++){
-        
-        if (min > arr[i])
-            max = arr[i];
-        
-    }
-    
-    cout << " the min number is " <<min << endl;
-    
     return 0;
-    
-    
-    
 }
